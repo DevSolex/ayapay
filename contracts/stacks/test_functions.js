46af4d8caa5174be4ba80b2d0e01c124f25cbe4b
Solex <solex@dev.com>
2026-05-20 22:15:27 +0100

    test: add test suite iteration 5 for add-employee and remove-employee

--- contracts/stacks/test_functions.js
+++ contracts/stacks/test_functions.js
@@ -91,3 +91,6 @@ console.log("Running test iteration 3 for add-employee and remove-employee");
 
 // Test suite iteration 4 
 console.log("Running test iteration 4 for add-employee and remove-employee");
+
+// Test suite iteration 5 
+console.log("Running test iteration 5 for add-employee and remove-employee");
